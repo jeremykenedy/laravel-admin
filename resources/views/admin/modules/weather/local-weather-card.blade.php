e8db90de9f40fd5a2fe924645b2e4561e9c22704
jeremykenedy <jeremy@grayboxpdx.com>
2016-02-23 22:04:28 -0800

    cleaup weather view a little bit

--- resources/views/admin/modules/weather/local-weather-card.blade.php
+++ resources/views/admin/modules/weather/local-weather-card.blade.php
@@ -1,9 +1,18 @@
-  <div class="row">
-    <div class="span12">
-      <div id="weather"></div>
+{{-- About Me Box --}}
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h3 class="box-title">
+          Your Weather
+        </h3>
+    <div class="box-tools pull-right">
+        <button class="btn btn-box-tool" title="" data-widget="collapse" data-toggle="tooltip" type="button" data-original-title="Collapse"><i class="fa fa-minus"></i></button>
+        <button class="btn btn-box-tool" title="" data-widget="remove" data-toggle="tooltip" type="button" data-original-title="close"><i class="fa fa-times"></i></button>
     </div>
-  </div>
-
+    </div>
+    <div class="box-body">
+        <div id="weather"></div>
+    </div>
+</div>
 
 
 @if ($user->profile->location)
